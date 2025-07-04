b03ea26d9227fc03fa6fec8d56fb39b5296ab3f5
irakoze kenny <irakozekenny09@gmail.com>
2025-07-04 09:56:45 +0200

    third commit

--- java.js
+++ java.js
@@ -1,1 +1,13 @@
-const icon = document.get
+// const icon = document.getElementById('icon')
+// const input = document.getElementById('input')
+// const chatting = document.getElementById('chatting')
+// icon.addEventListener('click' , (e) => {
+//     e.preventDefault
+//     const replies = document.createElement ('div');
+//     const chattingArea = chatting.value.trim;
+//     if (chattingArea === ""){
+//         replies.classList.add('few')
+//         replies. textContent = chatting;
+//     }
+//     chatting.appendChild(replies)
+// })
