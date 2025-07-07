d9ec07af4e0c07d97612db69e669b42dec23051d
irakoze kenny <irakozekenny09@gmail.com>
2025-07-07 11:50:47 +0200

    fourth commit

--- java.js
+++ java.js
@@ -1,13 +1,27 @@
-// const icon = document.getElementById('icon')
-// const input = document.getElementById('input')
-// const chatting = document.getElementById('chatting')
-// icon.addEventListener('click' , (e) => {
-//     e.preventDefault
-//     const replies = document.createElement ('div');
-//     const chattingArea = chatting.value.trim;
-//     if (chattingArea === ""){
-//         replies.classList.add('few')
-//         replies. textContent = chatting;
-//     }
-//     chatting.appendChild(replies)
-// })
+const icon = document.getElementById('icon');
+const input = document.getElementById('input');
+const chatting = document.getElementById('chatting');
+
+icon.addEventListener('click', (e) => {
+    e.preventDefault();
+    const messageText = input.value.trim();
+    if (messageText === "") 
+    return;
+    const messageContainer = document.createElement('div');
+    messageContainer.classList.add('right');
+
+    const message = document.createElement('div');
+    message.classList.add('replied', 'few');
+    
+    message.textContent = messageText;
+    messageContainer.appendChild(message);
+    chatting.appendChild(messageContainer);
+    input.value = '';
+    chatting.scrollTop = chatting.scrollHeight;
+});
+    
+    
+    
+    
+    
+    
